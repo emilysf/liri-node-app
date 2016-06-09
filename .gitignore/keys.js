65948d6a3948257ea99da67f24bf68fc69bb25c8
emilysf <emilysfrazin@gmail.com>
2016-06-09 12:58:23 -0400

    files added

---
+++ .gitignore/keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'xHBSH1ZgIrjSH1B40l5qJUe2n',
+  consumer_secret: 'e89lBm7WEbyIH2e3z8mS1oic12zSmSnKy9kNRtN4Jbceb4eJRt',
+  access_token_key: '166850182-0YpvIeq8d9EDSbs5PxllNYIRfAXwErFv6GJp86ty',
+  access_token_secret: '166850182-0YpvIeq8d9EDSbs5PxllNYIRfAXwErFv6GJp86ty',
+}

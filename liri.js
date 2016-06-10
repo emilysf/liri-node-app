0c2055e57811eb22e9278cff42b3542239257724
emilysf <emilysfrazin@gmail.com>
2016-06-10 18:10:18 -0400

    log.txt code was added/final commit

--- liri.js
+++ liri.js
@@ -33,20 +33,28 @@ function start(arg1,arg2) {
 function mytweets() {
 	//gets twitter app keys
 	var client = new Twitter(keys.twitterKeys);
+
 	var params = { screen_name: 'emsems12', count: 20 };
 	client.get('statuses/user_timeline', params , function(error, tweets, response) {
+		//writes out tweets in console
+	    if (!error) {
+	    	for (var i = 0; i < tweets.length; i++) {
+	  			console.log(tweets[i].created_at + " ");
+	  			console.log(tweets[i].text);
+	  			//writes tweets to log.txt
+	  			fs.appendFileSync("./log.txt",'\n-----My Tweets-----' + ' \n' + tweets[i].created_at + ' \n' + tweets[i].text + ' \n', 'utf8', function(err) {
+		    		if(err) {
+		       	 		console.log(JSON.stringify(err));
+		    		} 
+				});
+	  		}
+	    }
+	    else {
+	      	console.log(JSON.stringify(error));
+	    }
 
-    if (!error) {
-    	
-    	for (var i = 0; i < tweets.length; i++) {
-  			console.log(tweets[i].created_at + " ");
-  			console.log(tweets[i].text);
-  		}
-    }
-    else {
-      	console.log(JSON.stringify(error));
-    }
-  });
+  	});
+
 }
 //grabs spotify songs
 function spotifysong() {
@@ -58,16 +66,25 @@ function spotifysong() {
 	spotify.search({ type: 'track', query: arg2 }, function(err, data) {
 	    if ( !err ) {
 	    	// console.log(data);
+	    	var s = data.tracks.items[0];
 	    	console.log('-----Spotify Results-----')
-	        console.log('Artist(s): ' + data.tracks.items[0].artists[0].name)
-	        console.log('Song Name: ' + data.tracks.items[0].name);
-	        console.log('Preview Link: ' + data.tracks.items[0].preview_url);
-	        console.log('Album: ' + data.tracks.items[0].album.name);
+	        console.log('Artist(s): ' + s.artists[0].name)
+	        console.log('Song Name: ' + s.name);
+	        console.log('Preview Link: ' + s.preview_url);
+	        console.log('Album: ' + s.album.name);
+
 	    }
 	    else {
 	    	console.log(JSON.stringify(error));
 	        return;
 	    }
+
+	    //adds songs to log.txt
+        fs.appendFileSync("./log.txt", '\n' + '\n-----Spotify Results-----' + ' \n' + 'Artist(s): ' + s.artists[0].name + ' \n' + 'Song Name: ' + s.name + ' \n' + 'Preview Link: ' + s.preview_url + '\n' + 'Album: ' + s.album.name + '\n', 'utf8', function(err) {
+    		if(err) {
+       	 		console.log(JSON.stringify(err));
+    		} 
+		});
 	});
 }
 //grabs movie information from omdb
@@ -103,26 +120,32 @@ function moviethis() {
 
 	request(options, function(error, response, body) {
   		if (!error && response.statusCode == 200) {
-    		b = JSON.parse(body)
+    		m = JSON.parse(body)
     		console.log('-----OMDB Results-----')
-    		console.log('Title: ' + b.Title);
-    		console.log('Year: ' + b.Year);
-    		console.log('IMDB Rating: ' + b.imdbRating);
-    		console.log('Country: ' + b.Country);
-    		console.log('Language: ' + b.Language);
-    		console.log('Plot: ' + b.Plot);
-    		console.log('Actors: ' + b.Actors);
-    		console.log('Rotten Tomatoes Rating: ' + b.tomatoRating);
-    		console.log('Rotten Tomatoes URL: ' + b.tomatoURL);
+    		console.log('Title: ' + m.Title);
+    		console.log('Year: ' + m.Year);
+    		console.log('IMDB Rating: ' + m.imdbRating);
+    		console.log('Country: ' + m.Country);
+    		console.log('Language: ' + m.Language);
+    		console.log('Plot: ' + m.Plot);
+    		console.log('Actors: ' + m.Actors);
+    		console.log('Rotten Tomatoes Rating: ' + m.tomatoRating);
+    		console.log('Rotten Tomatoes URL: ' + m.tomatoURL);
   		}
   		else {
   			console.log(JSON.stringify(error));
 	        return;
   		}
+  		//writes omdb movie to log.txt
+  		fs.appendFileSync("./log.txt", '\n' + '\n-----OMDB Results----- ' + ' \n' + 'Title: ' + m.Title + ' \n' + 'Year: ' + m.Year + ' \n' + 'IMDB Rating: ' + m.imdbRating + '\n' + 'Country: ' + m.Country + ' \n' + 'Language: ' + m.Language + ' \n' + 'Plot: ' + m.Plot + ' \n' + 'Actors: ' + m.Actors + ' \n' + 'Rotten Tomatoes Rating: ' + m.tomatoRating + ' \n' + 'Rotten Tomatoes URL: ' + m.tomatoURL + ' \n', 'utf8', function(err) {
+    		if(err) {
+       	 		console.log(JSON.stringify(err));
+    		} 
+		});
   		
 	})
 }
-
+//do-what-it-says function grabs random.txt file data
 function dowhat() {
 
 	fs.readFile("random.txt", "utf8", function(error, data) {

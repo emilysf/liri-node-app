0168dfb57c97e746e2dc27b7d63ab8b4fdb4972a
emilysf <emilysfrazin@gmail.com>
2016-06-18 11:18:28 -0400

    deleted item

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: 'er1bSN14oBHFE5lsgGYejNAKz',
-  consumer_secret: 'ks9NoPJGzE9H0ZgZCDJNGoAhri5fEOf4PC8dasWCdxrCL2a2ni',
-  access_token_key: '166850182-j1ZxWLqpq1wtyN6L8Bl14o7bDYNGtFO4BXPFfmxn',
-  access_token_secret: '7KL64vtFi6Yw8p93lcYpbwpbsvobetW43qWY1tTuq4fsX',
-}
